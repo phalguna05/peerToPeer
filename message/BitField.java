08e8e1a95480430c4a190071f4c41b1732c47739
amruthavalliyvsr-create <144392046+amruthavalliyvsr-create@users.noreply.github.com>
2023-10-24 17:10:45 -0400

    Update BitField.java with readable comments

--- message/BitField.java
+++ message/BitField.java
@@ -10,21 +10,24 @@ public class BitField {
     public BitField() {
         Properties properties = new Properties();
         try {
+            //reading the Common Configuration file
             FileInputStream fileInputStream = new FileInputStream("Common.cfg");
             properties.load(fileInputStream);
             fileInputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //reading the FileSize from the Common Configuration file
         Double fileSize = Double.parseDouble(String.valueOf(properties.getProperty("FileSize")));
+        //reading the PieceSize from the Common Configuration file
         Double pieceSize = Double.parseDouble(String.valueOf(properties.getProperty("PieceSize")));
+        //Finding the Number of pieces in which the file will be sent
         numberOfPieces = (int) Math.ceil(fileSize / pieceSize);
+        //Creating piece objects
         pieces = new Piece[numberOfPieces];
+        //initializing each Piece object with content and piece index
         for (int i = 0; i < pieces.length; i++) {
             pieces[i] = new Piece();
-            //System.out.println(pieces[i].getContent());
-           // System.out.println(pieces[i].getPieceIndex());
-
         }
 }
 /*public static void main(String[] args) {
